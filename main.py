81223e418610ae17733507f513f7dd547fc01311
LinhHoang2812 <zoeylinh2812@gmail.com>
2023-03-27 23:55:29 +0200

    replace sqlite with postgresql

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, cur
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -18,7 +19,10 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["DATABASE_URL"]
+
+#'sqlite:///blog.db'
+
 app.app_context().push()
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -73,7 +77,7 @@ class Comment(db.Model):
 
 
 
-#db.create_all()
+db.create_all()
 
 gravatar = Gravatar(app,
                     size=100,
